5d046df47bae311d81be4199c9c8c2b92a10a6f1
kgurushankar773 <kgurushankar773@PR28.hhs.win.fuhsd.org>
2017-02-28 15:28:53 -0800

    Finished Vendor and minor bug fixes

--- src/snackbar/VendingMachine.java
+++ src/snackbar/VendingMachine.java
@@ -1,3 +1,5 @@
+package snackbar;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
--- src/snackbar/Vendor.java
+++ src/snackbar/Vendor.java
@@ -1,85 +1,51 @@
-/*
-   This class implements a vendor that sells one kind of items.
-   A vendor carries out sales transactions.
-*/
+package snackbar;
 
-public class Vendor
-{
-  // Fields:
-  ...
+/**
+ * This class implements a vendor that sells one kind of items. A vendor carries
+ * out sales transactions.
+ */
 
-  //  Constructor
-  //  Parameters:
-  //    int price of a single item in cents
-  //    int number of items to place in stock
-  ... Vendor ...
-  {
-    ...
-  }
+public class Vendor {
+	private int price;
+	private int stock;
+	private int moneyInserted;
+	private int change;
 
-  //  Sets the quantity of items in stock.
-  //  Parameters:
-  //    int number of items to place in stock
-  //  Return:
-  //    None
-  ... setStock ...
-  {
-    ...
-  }
+	public Vendor(int price, int stock) {
+		this.price = price;
+		this.stock = stock;
+		this.moneyInserted = 0;
+	}
 
-  //  Returns the number of items currently in stock.
-  //  Parameters:
-  //    None
-  //  Return:
-  //    int number of items currently in stock
-  ... getStock ...
-  {
-    ...
-  }
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
 
-  //  Adds a specified amount (in cents) to the deposited amount.
-  //  Parameters:
-  //    int number of cents to add to the deposit
-  //  Return:
-  //    None
-  ... addMoney ...
-  {
-    ...
-  }
+	public int getStock() {
+		return stock;
+	}
 
-  //  Returns the currently deposited amount (in cents).
-  //  Parameters:
-  //    None
-  //  Return:
-  //    int number of cents in the current deposit
-  ... getDeposit ...
-  {
-    ...
-  }
+	public void addMoney(int cents) {
+		this.moneyInserted += cents;
+	}
 
-  //  Implements a sale.  If there are items in stock and
-  //  the deposited amount is greater than or equal to
-  //  the single item price, then adjusts the stock
-  //  and calculates and sets change and returns true;
-  //  otherwise refunds the whole deposit (moves it into change)
-  //  and returns false.
-  //  Parameters:
-  //    None
-  //  Return:
-  //    boolean successful sale (true) or failure (false)
-  ... makeSale ...
-  {
-    ...
-  }
+	public int getDeposit() {
+		return this.moneyInserted;
+	}
 
-  //  Returns and zeroes out the amount of change (from the last
-  //  sale or refund).
-  //  Parameters:
-  //    None
-  //  Return:
-  //    int number of cents in the current change
-  ... getChange ...
-  {
-    ...
-  }
+	public boolean makeSale() {
+		if (this.moneyInserted >= this.price && stock > 0) {
+			this.stock--;
+			this.change = this.moneyInserted - this.price;
+			return true;
+		} else {
+			this.change = this.moneyInserted;
+			return false;
+		}
+	}
+
+	public int getChange() {
+		this.moneyInserted = 0;
+		return this.change;
+	}
 }
